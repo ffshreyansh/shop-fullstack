1aaa06592f84ff767cc234159f33e0ad732570f8
Shreyansh Kumar <shreyanshkumar058@gmail.com>
2023-12-02 11:22:43 +0530

    main page

--- components/Home.jsx
+++ components/Home.jsx
@@ -3,14 +3,26 @@ import React from 'react'
 const Home = () => {
     return (
         <>
-            <div className='flex items-center justify-between w-full gap-4'>
-                <div className='h-fit w-full lg:w-1/2 flex flex-col gap-4'>
-                    <p className='text-lg w-1/2 text-gray-400'>Absolutely hot collections 🔥</p>
-                    <h1 className='text-6xl w-3/4 font-bold'>The Best Place to Find And Buy <span><img className=' w-auto inline-block rotate-180' src="https://img.icons8.com/ios/50/000000/curly-arrow--v1.png" alt="curly-arrow--v1"/></span> Amazing <span className='gP'>Products</span></h1>
-                    <p className='w-2/3 text-gray-400'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore, deserunt. Lorem ipsum dolor sit amet.</p>
-                    <button className='text-md font-semibold text-white bg-black w-1/4 py-5 rounded-xl'>Explore Now</button>
+            <div className='flex items-center justify-between w-full relative'>
+                <div className='h-fit w-full lg:w-2/3 flex flex-col pl-10 gap-8'>
+                    <p className='text-lg w-1/2 text-gray-400 font-medium'>Absolutely hot collections 🔥</p>
+                    <h1 className=' text-7xl w-4/5 font-bold'>The Best Place to Find And Buy <span><img className=' w-auto inline-block rotate-180' src="https://img.icons8.com/ios/50/000000/curly-arrow--v1.png" alt="curly-arrow--v1" /></span> Amazing <span className='gP'>Products</span></h1>
+                    <p className='w-2/3 text-gray-400 text-xl'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore, deserunt. Lorem ipsum dolor sit amet.</p>
+                    <button className='text-md font-semibold text-white bg-black w-1/5 py-5 rounded-xl'>Explore Now</button>
+                    <div className='flex items-center gap-7'>
+                        <div className='flex flex-col items-start'>
+                            <span className='text-2xl font-bold'>20k+</span>
+                            <span className='text-xs font-medium'>Collections</span>
+                        </div>
+                        <span className=' text-gray-300'>|</span>
+                        <div className='flex flex-col items-start'>
+                            <span className='text-2xl font-bold'>16k</span>
+                            <span className='text-xs font-medium'>Users</span>
+                        </div>
+                    </div>
                 </div>
-                <div className='h-fit w-full lg:w-1/2 flex flex-col gap-7'>
+                <img src="/assets/rtt-01.png" class='rotate-image absolute'  alt="" />
+                <div className='h-fit w-full lg:w-1/3 flex flex-col gap-7'>
                     <div className='hM'>
                         <img
                             src="/assets/icons/h1.jpg"
@@ -18,25 +30,11 @@ const Home = () => {
                             className="w-full h-full object-cover"
                         />
                     </div>
-                    
+
                 </div>
 
             </div>
-            <div className='mt-20'>
-                <div className='hM relative'>
-                    <img
-                        src="https://images.pexels.com/photos/63776/pexels-photo-63776.jpeg"
-                        alt=""
-                        className="w-full h-full object-cover object-top"
-                    />
-                    <div className='absolute top-1/3 right-20 w-60'>
-                        <p className='font-semibold mb-1'>Leather Bag</p>
-                    <p className='text-sm mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet.</p>
-                    <button className=" border-2 border-black font-semibold px-6 py-2 w-2/3 hover:bg-black hover:text-white transition-all">Shop now</button>
-                    </div>
-                </div>
-
-            </div>
+          
         </>
 
     )
